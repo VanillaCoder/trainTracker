9c5be1e7797ded0099e5144648d84d757f0e9174
LiamH <liamhearts@gmail.com>
2019-04-22 17:01:03 -0700

    form now sends data to database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,10 @@
+//TODO: Convert first train time to unix
+//TODO: Package and send the data to database
+//TODO: Take data from database and display it in table
+//TODO: calculate the time unil the next train and send it to the table
+//TODO: Be happy this shit is finally over, enjoy a beer.
+
+
 var config = {
     apiKey: "AIzaSyDcQzq1LS42JUzxk0evD5z6N5nWk_E5cg0",
     authDomain: "projectvanilla-155df.firebaseapp.com",
@@ -5,9 +12,31 @@ var config = {
     projectId: "projectvanilla-155df",
     storageBucket: "projectvanilla-155df.appspot.com",
     messagingSenderId: "44997861253"
-  };
-  firebase.initializeApp(config);
-  
-  // Assign the reference to the database to a variable named 'database'
-  // var database = ...
-  var database = firebase.database()
+};
+firebase.initializeApp(config);
+
+// Assign the reference to the database to a variable named 'database'
+// var database = ...
+var database = firebase.database()
+
+
+
+//TODO: Capture values from input form
+$("#btn-submit").on("click", function () {
+    event.preventDefault()
+    var tName = $("#train-name").val().trim()
+    var tDestination = $("#train-destination").val().trim()
+    var tFirst = $("#train-first").val().trim()
+    var tFrequency = $("#train-frequency").val().trim()
+
+
+    database.ref().push({
+        Name: tName,
+        Destination: tDestination,
+        First: tFirst,
+        Frequency: tFrequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+    })
+
+})
